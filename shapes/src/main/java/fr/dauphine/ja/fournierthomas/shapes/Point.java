2036e6cc6f2c37f803c1683ae2a570140d7030e2
fourth16 <fourth16@b028-05.crio.dauphine.fr>
2019-10-07 14:12:56 +0200

    shapes init

---
+++ shapes/src/main/java/fr/dauphine/ja/fournierthomas/shapes/Point.java
@@ -0,0 +1,12 @@
+package fr.dauphine.ja.fournierthomas.shapes;
+import java.util.*;
+
+public class Point{
+	private int x,y;
+	
+	public static void main( String[] args ) {
+		Point p=new Point();
+		System.out.println(p.x+" "+p.y);
+	}
+	
+}
